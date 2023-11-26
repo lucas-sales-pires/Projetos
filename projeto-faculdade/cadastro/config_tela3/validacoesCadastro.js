aa59eca7e0df68ce785b1a895f800e045878ac58
lucas sales <lucas2013sales@hotmail.com>
2023-11-26 08:04:20 -0300

    atualizações para entrega

--- projeto-faculdade/cadastro/config_tela3/validacoesCadastro.js
+++ projeto-faculdade/cadastro/config_tela3/validacoesCadastro.js
@@ -1,5 +1,4 @@
 
-require =('../../geral/converterParaMaiusculo');
 
 
 function validarNome(){
---
+++ projetoValueWare/javaScript/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#carouselExampleSlidesOnly').carousel({
+      interval: 5000, // intervalo em milissegundos entre cada deslizamento
+    });
+  });
+  
