a7d7064660dd780339088c92cb24e89f8c1cbc83
lucas sales <lucas2013sales@hotmail.com>
2024-01-29 11:20:19 -0300

    novo projeto front end

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require("gulp")
+const cssnano = require("gulp-cssnano")
+const htmlmin = require("gulp-htmlmin")
+const concat = require("gulp-concat")
+
+
+function minificarCSS() {
+    return gulp.src("src/**/*.css")
+    .pipe(cssnano())
+    .pipe(gulp.dest("build/assets/css"))
+}
+function minificarHTML() {
+    return gulp.src("src/**/*.html")    
+    .pipe(concat("index.min.html"))
+    .pipe(htmlmin({
+        collapseWhitespace:true,
+        removeComments:true
+    }))
+    .pipe(gulp.dest("build"))
+}
+
+function watch(){
+    gulp.watch("src/**/*.css",gulp.series(minificarCSS))
+    gulp.watch("src/**/*.html",gulp.series(minificarHTML))
+}
+
+gulp.task("default",gulp.series(minificarHTML,minificarCSS,watch))
