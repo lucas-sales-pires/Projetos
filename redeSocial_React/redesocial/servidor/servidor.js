393f0ece2896bf3dfa887f28c31064067fe55ccb
lucas-sales-pires <lucas2013sales@hotmail.com>
2024-03-26 10:29:11 -0300

    Update servidor.js

--- redeSocial_React/redesocial/servidor/servidor.js
+++ redeSocial_React/redesocial/servidor/servidor.js
@@ -7,7 +7,7 @@ import routerEmail from './emailRotas.js';
 const app = express();
 const port = 3001;
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(cors())
 app.use('/autenticacao', routeraut);
 app.use('/recuperar-senha', routerrec);
